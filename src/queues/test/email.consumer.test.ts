86e70980d8bff0d149a2ff5e9247ec104fb0ecb2
Diego <diego2000avelar@gmail.com>
2024-01-12 08:40:47 -0600

    fix: test suites

--- src/queues/test/email.consumer.test.ts
+++ src/queues/test/email.consumer.test.ts
@@ -1,6 +1,6 @@
 import * as connection from '@notifications/queues/connection';
 import amqp from 'amqplib';
-import { consumeAuthEmailMessage } from '@notifications/queues/email.consumer';
+import { consumeAuthEmailMessage, consumeOrderEmailMessage } from '@notifications/queues/email.consumer';
 
 jest.mock('@notifications/queues/connection');
 jest.mock('amqplib');
@@ -29,6 +29,30 @@ describe('Email Consumer', () => {
       jest.spyOn(connection, 'createConnection').mockReturnValue(channel as never);
       const connectionChannel: amqp.Channel | undefined = await connection.createConnection();
       await consumeAuthEmailMessage(connectionChannel!);
+      expect(connectionChannel!.assertExchange).toHaveBeenCalledWith('jobber-email-notification', 'direct');
+      expect(connectionChannel!.assertQueue).toHaveBeenCalledTimes(1);
+      expect(connectionChannel!.bindQueue).toHaveBeenCalledWith('auth-email-queue', 'jobber-email-notification', 'auth-email');
     });
   });
+
+  describe('consumeOrderEmailMessage method', () => {
+    it('should be called', async () => {
+      const channel = {
+        assertExchange: jest.fn(),
+        publish: jest.fn(),
+        assertQueue: jest.fn(),
+        bindQueue: jest.fn(),
+        consume: jest.fn()
+      };
+      jest.spyOn(channel, 'assertExchange');
+      jest.spyOn(channel, 'assertQueue').mockReturnValue({ queue: 'order-email-queue', messageCount: 0, consumerCount: 0 });
+      jest.spyOn(connection, 'createConnection').mockReturnValue(channel as never);
+      const connectionChannel: amqp.Channel | undefined = await connection.createConnection();
+      await consumeOrderEmailMessage(connectionChannel!);
+      expect(connectionChannel!.assertExchange).toHaveBeenCalledWith('jobber-order-notification', 'direct');
+      expect(connectionChannel!.assertQueue).toHaveBeenCalledTimes(1);
+      expect(connectionChannel!.bindQueue).toHaveBeenCalledWith('order-email-queue', 'jobber-order-notification', 'order-email');
+    });
+  });
+
 });
